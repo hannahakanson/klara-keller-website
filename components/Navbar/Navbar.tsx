b68bf9706e2fd30414f9d418a27e48f461c653f7
Hanna Håkanson <hannahakanson@live.se>
2023-01-03 20:47:24 +0100

    #5 minor changes to navbar styling

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import navbarDiskImage from "../../public/Music-disc.ico"
+import navbarDiskImage from "../../public/favicon.ico"
 import CPmenuImage from "../../public/CP-menu.ico"
 
 import localFont from "@next/font/local";
 
 const myFont = localFont({
-  src: "../../public/W95FA.otf",
+  src: "../../public/fonts/W95FA.otf",
 });
 
 
@@ -15,10 +15,12 @@ const Navbar = () => {
   return (
     <header className="font-display border-b border-red-300 flex items-center justify-between mb-1 space-x-2 pr-5 py-5">
       <div className="pl-5">
-        <Link className={myFont.className} href="/">KLARA KELLER</Link>
+        <Link className={myFont.className} href="/">
+          KLARA KELLER
+        </Link>
       </div>
 
-      <div>
+      <div className={myFont.className}>
         <ul className="hidden justify-between space-x-12 pr-5 py-5 md:flex md:items-center md:text-sm">
           <li>Home</li>
           <li>About</li>
