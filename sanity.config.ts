988cd14054b33755a0713246e977978a0a925c83
Hanna Håkanson <hannahakanson@live.se>
2023-01-03 11:37:15 +0100

    #2 Set up sanity backend

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,21 +1,20 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineConfig({
   basePath: "/studio",
-  name: 'default',
-  title: 'klara-keller-sanity-project',
-  projectId: projectId,
-  dataset: dataset,
-
+  name: "klara_keller_content_studio",
+  title: "Klara Keller Content Studio",
+  projectId,
+  dataset,
   plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
