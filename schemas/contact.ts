70e27dc8ce1552249175cd23b3536d31e9cee1e7
Hanna Håkanson <hannahakanson@live.se>
2023-01-04 14:50:20 +0100

    #7 Added contact schema

---
+++ schemas/contact.ts
@@ -0,0 +1,42 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "contact",
+  title: "Contact info",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+    }),
+    defineField({
+      name: "email",
+      title: "Email",
+      type: "string",
+    }),
+    defineField({
+      name: "number",
+      title: "Phone number",
+      type: "string",
+    }),
+    defineField({
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: { type: "author" },
+    }),
+    defineField({
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+    },
+  },
+});
