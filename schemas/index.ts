070d0e4aafa1f5f7fc57c08962e16424860cea09
Hanna Håkanson <hannahakanson@live.se>
2023-01-04 14:24:12 +0100

    #7 Added schema for concert

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,7 @@
 import blockContent from './blockContent'
 import heroContent from './heroContent'
 import newsContent from './newsContent'
+import concert from './concert'
 import author from './author'
 
-export const schemaTypes = [heroContent, newsContent, author, blockContent]
+export const schemaTypes = [heroContent, newsContent, concert, author, blockContent]
