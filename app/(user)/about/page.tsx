f38aaabc5db0bd32e082eed7107d5320909143b0
Hanna Håkanson <hannahakanson@live.se>
2023-01-04 15:10:51 +0100

    #5 Added routes for pages

---
+++ app/(user)/about/page.tsx
@@ -0,0 +1,6 @@
+
+export default async function AboutPage() {
+  //IF YOU'RE IN PREVIEW MODE
+
+  return <div>ABOUT PAGE</div>;
+}
---
+++ app/(user)/music/page.tsx
@@ -0,0 +1,8 @@
+
+export default async function MusicPage() {
+  //IF YOU'RE IN PREVIEW MODE
+  
+    return (
+      <div>MUSIC PAGE</div>
+    );
+}
