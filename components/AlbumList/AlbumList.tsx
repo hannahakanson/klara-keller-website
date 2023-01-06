be8bfece3a7a0448bc6ca1f88c2ec7a0efdfe554
Hanna Håkanson <hannahakanson@live.se>
2023-01-06 15:43:20 +0100

    #18 Added typing and component for album list

---
+++ components/AlbumList/AlbumList.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import makeUrl from "../../lib/helpers";
+import { AlbumListProps } from "./AlbumList.types";
+
+const AlbumList = ({ albums }: AlbumListProps) => {
+  return (
+    <div>
+      {albums.map((album) => (
+        <Link href={album.link}>
+          <div key={album._id} className="flex flex-col pb-4">
+            <div className="text-xs">{album.title}</div>
+            <div className="relative w-full h-60 my-6">
+              <Image
+                src={makeUrl(album.mainImage).url()}
+                alt={album.title}
+                className="object-contain"
+                fill
+              />
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default AlbumList;
---
+++ components/AlbumList/AlbumList.types.ts
@@ -0,0 +1,5 @@
+import { Album } from "../../typings";
+
+export type AlbumListProps = {
+  albums: Album[];
+};
