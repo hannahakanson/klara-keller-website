0f12687a55bf346519525177c95ede7df4ab9ef1
Hanna Håkanson <hannahakanson@live.se>
2023-01-08 00:32:31 +0100

    #17 Additional styling for contact

--- components/ContactList/ContactList.tsx
+++ components/ContactList/ContactList.tsx
@@ -2,14 +2,17 @@ import { ContactListProps } from "./ContactList.types";
 
 const ContactList = ({ contacts }: ContactListProps) => {
   return (
-    <div>
+    <div className="w-full md:w-2/3 flex flex-col">
       {contacts.map((contact) => (
-        <div key={contact._id} className="flex flex-col pb-4">
-          <div className="text-xs">{contact.title}</div>
-          <div>{contact.name}</div>
-          <div className="text-xs">{contact.email}</div>
-          <div className="text-xs">{contact.number}</div>
-        </div>
+        <div key={contact._id}>
+          <div className="divider text-xs uppercase">{contact.title}</div>
+         
+            <div className="w-full flex flex-col justify-between p-4">
+              <div className="text-xl">{contact.name}</div>
+              <div>{contact.email}</div>
+              <div>{contact.number}</div>
+            </div>
+          </div>
       ))}
     </div>
   );
