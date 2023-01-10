7984237cf32b51592159e72683f3742e71f1da31
Hanna Håkanson <hannahakanson@live.se>
2023-01-10 13:26:09 +0100

    #21

--- components/NewsList/NewsList.tsx
+++ components/NewsList/NewsList.tsx
@@ -1,28 +1,18 @@
-import Image from "next/image";
-
-import makeUrl from "../../lib/helpers";
 import { NewsListProps } from "./NewsList.types";
 
 const NewsList = ({ news }: NewsListProps) => {
-  console.log(news.length);
 
   return (
     <div>
       {/* News */}
       {news.map((news) => (
-        <div key={news._id}>
-          <div className="relative w-full h-60 my-6">
+        <div key={news._id} className="relative w-full my-6">
+          <div className="relative w-full display:flex flex-col">
+            <div className="text-xs opacity-50">{news._updatedAt}</div>
             <h2>{news.title}</h2>
             <p className="text-sm">
               {news.body[0].children.map((inline) => inline.text)}
             </p>
-            <Image
-              src={makeUrl(news.mainImage).url()}
-              alt={news.title}
-              className="object-contain"
-              fill
-            />
-            <div className="text-xs">{news._updatedAt}</div>
           </div>
         </div>
       ))}
