1dc1977315a50e77f307663ed45d58083655abfa
Hanna Håkanson <hannahakanson@live.se>
2023-01-11 11:23:28 +0100

    #31 Fixed font

--- components/ContactList/ContactList.tsx
+++ components/ContactList/ContactList.tsx
@@ -5,14 +5,16 @@ const ContactList = ({ contacts }: ContactListProps) => {
     <div className="w-full flex flex-col">
       {contacts.map((contact) => (
         <div key={contact._id}>
-          <div className="divider text-xs uppercase">{contact.title}</div>
-         
-            <div className="w-full flex flex-col justify-between p-4">
-              <div className="text-xl">{contact.name}</div>
-              <div>{contact.email}</div>
-              <div>{contact.number}</div>
-            </div>
+          <div className="divider text-s uppercase">
+            <h2>{contact.title}</h2>
           </div>
+
+          <div className="w-full flex flex-col justify-between p-4">
+            <div className="text-xl">{contact.name}</div>
+            <div>{contact.email}</div>
+            <div>{contact.number}</div>
+          </div>
+        </div>
       ))}
     </div>
   );
