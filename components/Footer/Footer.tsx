8b4bb2292d343d5a97acc06445a7d80b1071524b
Hanna Håkanson <hannahakanson@live.se>
2023-01-11 13:29:23 +0100

    #10 Basic design of footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,13 +2,15 @@ import { BsSpotify, BsFacebook, BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
     return (
-      <footer className="bg-gray-800 p-4 bottom-0 w-full">
-        <div className="container gap-4 flex justify-center mx-auto items-center">
-          <BsInstagram size={40} />
-          <BsSpotify size={40} />
-          <BsFacebook size={40} />
-        </div>
-      </footer>
+      <div className="flex mt-6">
+        <footer className="bg-klara-purple p-4 bottom-0 w-screen md:bg-transparent">
+          <div className="gap-4 flex justify-center mx-auto items-center">
+            <BsInstagram size={40} />
+            <BsSpotify size={40} />
+            <BsFacebook size={40} />
+          </div>
+        </footer>
+      </div>
     );
 }
 
