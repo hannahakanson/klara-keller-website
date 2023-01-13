8d5f7b2f021c727f71d140d61277871af98fd71c
Hanna Håkanson <hannahakanson@live.se>
2023-01-13 10:50:55 +0100

    #6 Added preview in sanity studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
+import { getDefaultDocumentNode } from "./previewStructure";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
@@ -12,7 +13,9 @@ export default defineConfig({
   title: "Klara Keller Content Studio",
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool({defaultDocumentNode: getDefaultDocumentNode}
+
+  ), visionTool()],
 
   schema: {
     types: schemaTypes,
